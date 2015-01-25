038c3eb0c94ca23c5514874e52923106828f02a0
Linas Vepstas <linasvepstas@gmail.com>
2015-01-25 14:55:48 -0600

    use  stderr for error messages

--- src/protocol.c
+++ src/protocol.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
-hid_device *openHID(int vid,int pid){
+hid_device *openHID(int vid,int pid) {
 	wchar_t wstr[MAX_STR];
 	hid_device *handle;
 	int res;
@@ -15,7 +15,7 @@ hid_device *openHID(int vid,int pid){
 
 	handle = hid_open(vid, pid, NULL);
 	if (!handle) {
-		printf("unable to open device\n");
+		fprintf(stderr, "Error: unable to open device\n");
  		return NULL;
 	}
 
@@ -23,21 +23,21 @@ hid_device *openHID(int vid,int pid){
 	wstr[0] = 0x0000;
 	res = hid_get_manufacturer_string(handle, wstr, MAX_STR);
 	if (res < 0)
-		printf("Unable to read manufacturer string\n");
+		fprintf(stderr, "Error: Unable to read manufacturer string\n");
 	//printf("Manufacturer String: %ls\n", wstr);
 
 	// Read the Product String
 	wstr[0] = 0x0000;
 	res = hid_get_product_string(handle, wstr, MAX_STR);
 	if (res < 0)
-		printf("Unable to read product string\n");
+		fprintf(stderr, "Error: Unable to read product string\n");
 	//printf("Product String: %ls\n", wstr);
 
 	// Read the Serial Number String
 	wstr[0] = 0x0000;
 	res = hid_get_serial_number_string(handle, wstr, MAX_STR);
 	if (res < 0)
-		printf("Unable to read serial number string\n");
+		fprintf(stderr, "Error: Unable to read serial number string\n");
 	//printf("Serial Number String: (%d) %ls", wstr[0], wstr);
 	printf("\n");
 
@@ -45,7 +45,7 @@ hid_device *openHID(int vid,int pid){
 	wstr[0] = 0x0000;
 	res = hid_get_indexed_string(handle, 1, wstr, MAX_STR);
 	if (res < 0)
-		printf("Unable to read indexed string 1\n");
+		fprintf(stderr, "Error: Unable to read indexed string 1\n");
 	//printf("Indexed String 1: %ls\n", wstr);
 
 	// Set the hid_read() function to be non-blocking.
@@ -62,18 +62,20 @@ hid_device *openHID(int vid,int pid){
 	buf[4] = 0x00;
 	res = hid_send_feature_report(handle, buf, 17);
 	if (res < 0) {
-		printf("Unable to send a feature report.\n");
+		fprintf(stderr, "Error: Unable to send a feature report.\n");
 	}
 	return handle;
 }
 
-void closeHID(hid_device *handle){
+void closeHID(hid_device *handle)
+{
 	hid_close(handle);
 	/* Free static HIDAPI objects. */
 	hid_exit();
 }
 
-int poolPresence(hid_device *handle){
+int poolPresence(hid_device *handle)
+{
 	unsigned char buf[256];
 	memset(buf,0,sizeof(buf));
 	int res;
@@ -82,14 +84,14 @@ int poolPresence(hid_device *handle){
 	buf[0] = 0x2;
 	res = hid_get_feature_report(handle, buf, sizeof(buf));
 	if (res < 0) {
-		printf("Unable to get a feature report.\n");
-		printf("%ls", hid_error(handle));
+		fprintf(stderr, "Error: Unable to get a feature report.\n");
+		fprintf(stderr, "%ls", hid_error(handle));
 	}
-	if(buf[2]==0x02 && buf[3]==0x0b){
-		printf("HRM not present.\n");
+	if (buf[2]==0x02 && buf[3]==0x0b){
+		fprintf(stderr, "Error: HRM not present.\n");
 		return 0;
 	}
-	//printf("HRM present.\n");
+	fprintf(stderr, "HRM detected.\n");
 	return 1;
 }
 
@@ -104,16 +106,17 @@ int executeCommand1(hid_device *handle, unsigned char *buf, int bufsize, unsigne
 	}
 	res = hid_write(handle, buf, 17);
 	if (res < 0) {
-		printf("Unable to write()\n");
-		printf("Error: %ls\n", hid_error(handle));
+		fprintf(stderr, "Error: Unable to write()\n");
+		fprintf(stderr, "Error: %ls\n", hid_error(handle));
 		return 0;
 	}
-	res=readData(handle,buf,bufsize, showdata,0);
+	res = readData(handle,buf,bufsize, showdata,0);
 	return res;
 
 }
 
-int readData(hid_device *handle, unsigned char *buf, int bufsize, int showdata,int offset){
+int readData(hid_device *handle, unsigned char *buf, int bufsize, int showdata,int offset)
+{
 	int i,res;
 
 	memset(buf,0,bufsize*sizeof(unsigned char));
@@ -127,7 +130,7 @@ int readData(hid_device *handle, unsigned char *buf, int bufsize, int showdata,i
 			usleep(500*1000);
 		}
 		if (res < 0)
-			printf("Unable to read()\n");
+			fprintf(stderr, "Error: Unable to read()\n");
 	}
 	if (res<=0) {
 		//printf("No data received!\n");
@@ -139,7 +142,4 @@ int readData(hid_device *handle, unsigned char *buf, int bufsize, int showdata,i
 		printf("\n");
 	}
 	return res;
-
-
 }
-
